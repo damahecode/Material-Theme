3674d24ca6ada1e9e4e08d545a28637b4c6baf6d
Damahe Code <damahecode@gmail.com>
2024-02-16 11:19:17 +0530

    new class

---
+++ app/src/main/java/com/code/damahe/app/Activity.kt
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2024 damahecode.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package com.code.damahe.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.viewModels
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.code.damahe.material.theme.DCodeAppTheme
+import com.code.damahe.material.utils.ThemeUtil
+import com.code.damahe.material.viewmodel.ThemeUiState
+import com.code.damahe.material.viewmodel.ThemeViewModel
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+abstract class Activity : ComponentActivity() {
+
+    private val viewModel: ThemeViewModel by viewModels()
+    protected var themeUiState: ThemeUiState by mutableStateOf(ThemeUiState.Loading)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Update the uiState
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.themeUiState.onEach {
+                    themeUiState = it
+                }
+                    .collect()
+            }
+        }
+    }
+}
+
+/**
+ * Your Main Composable Function here
+ */
+@Composable
+fun MainContent(themeUiState: ThemeUiState, content: @Composable () -> Unit) {
+    val systemUiController = rememberSystemUiController()
+    val darkTheme = ThemeUtil.shouldUseDarkTheme(themeUiState)
+
+    // Update the dark content of the system bars to match the theme
+    DisposableEffect(systemUiController, darkTheme) {
+        systemUiController.systemBarsDarkContentEnabled = !darkTheme
+        onDispose {}
+    }
+
+    DCodeAppTheme(
+        darkTheme = darkTheme,
+        themeBrand = ThemeUtil.shouldUseOtherThemeBrand(themeUiState),
+        disableGradientColors = ThemeUtil.shouldDisableGradientColors(themeUiState),
+    ) {
+        content()
+    }
+}
---
+++ material-theme/src/main/java/com/code/damahe/material/model/ColorShades.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2024 damahecode.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package com.code.damahe.material.model
+
+import androidx.compose.ui.graphics.Color
+
+data class ColorShades(
+    val extraLight: Color,
+    val light: Color,
+    val kindaLight: Color,
+    val primary: Color,
+    val kindaDark: Color,
+    val dark: Color,
+    val extraDark: Color,
+)
