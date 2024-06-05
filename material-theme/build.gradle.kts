ebe14a80930b6e690d00bc467f050934c84987de
Damahe Code <damahecode@gmail.com>
2024-06-05 11:41:22 +0530

    removed unnecessary library

--- material-theme/build.gradle.kts
+++ material-theme/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
 
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.accompanist.systemuicontroller)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.datastore.preferences)
