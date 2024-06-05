aa751796c87a9e04299d88d7c81298d1cbfe3bcb
Damahe Code <damahecode@gmail.com>
2024-06-05 11:41:13 +0530

    removed unnecessary library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,7 +91,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     debugImplementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.accompanist.systemuicontroller)
 
     implementation(libs.hilt.android)
     ksp(libs.hilt.android.compiler)
