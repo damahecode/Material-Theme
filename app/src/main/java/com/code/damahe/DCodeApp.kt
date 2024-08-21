678379b7a80c5cc29b4800148ec5c0d969ed8d08
Damahe Code <damahecode@gmail.com>
2024-08-21 12:43:19 +0530

    removed unnecessary code

--- app/src/main/java/com/code/damahe/DCodeApp.kt
+++ app/src/main/java/com/code/damahe/DCodeApp.kt
@@ -9,9 +9,4 @@ import dagger.hilt.android.HiltAndroidApp
 @HiltAndroidApp
 class DCodeApp : Application() {
 
-    override fun onCreate() {
-        super.onCreate()
-        // Initialize Sync; the system responsible for keeping data in the app up to date.
-
-    }
 }
