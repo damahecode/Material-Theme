dd2df5e3ae2841a6c607d7c258753a1886970e70
Damahe Code <damahecode@gmail.com>
2025-02-01 17:15:49 +0530

    Enable Hilt for dependency injection
    The `@HiltAndroidApp` annotation is added to `DCodeApp` class to enable Hilt's code generation for dependency injection. The `onCreate` method is added to initialize application resources, if needed.

--- app/src/main/java/com/code/damahe/DCodeApp.kt
+++ app/src/main/java/com/code/damahe/DCodeApp.kt
@@ -4,9 +4,20 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 /**
- * [Application] class for Damahe Code
+ * The main [Application] class for the Damahe Code application.
+ *
+ * This class initializes the application and sets up the Hilt dependency injection framework.
+ * It serves as the application's entry point and is responsible for managing the application's
+ * global state and resources.
+ *
+ * The `@HiltAndroidApp` annotation triggers Hilt's code generation, creating the necessary
+ * components for dependency injection throughout the application.
  */
 @HiltAndroidApp
 class DCodeApp : Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+        // Initialization logic can be added here if needed.
+    }
 }
