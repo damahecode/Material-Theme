de5f623faa63c2820779af4b2d417262851945ad
Damahe Code <damahecode@gmail.com>
2025-02-01 17:13:56 +0530

    Refactor: Migrate Type.kt to core module and apply custom font
    - Moved `Type.kt` from `material-theme` to the `core` module.
    - Added custom font `JostFont` and applied it to all text styles in `DCodeTypography`.
    - Set custom `lineHeightStyle` for specific styles in `DCodeTypography`.

--- core/material-theme/src/main/java/com/code/damahe/material/theme/Type.kt
+++ core/material-theme/src/main/java/com/code/damahe/material/theme/Type.kt
@@ -2,101 +2,156 @@ package com.code.damahe.material.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.LineHeightStyle.Alignment
+import androidx.compose.ui.text.style.LineHeightStyle.Trim
 import androidx.compose.ui.unit.sp
+import com.code.damahe.material.R
+
+// Custom font
+val JostFont = FontFamily(
+    Font(R.font.jost_book),
+//    Font(R.font.jost_medium, FontWeight.Medium)
+)
 
 /**
  * Material Theme typography.
  */
 internal val DCodeTypography = Typography(
     displayLarge = TextStyle(
+        fontFamily = JostFont,
         fontWeight = FontWeight.Normal,
         fontSize = 57.sp,
         lineHeight = 64.sp,
         letterSpacing = (-0.25).sp,
     ),
     displayMedium = TextStyle(
+        fontFamily = JostFont,
         fontWeight = FontWeight.Normal,
         fontSize = 45.sp,
         lineHeight = 52.sp,
         letterSpacing = 0.sp,
     ),
     displaySmall = TextStyle(
+        fontFamily = JostFont,
         fontWeight = FontWeight.Normal,
         fontSize = 36.sp,
         lineHeight = 44.sp,
         letterSpacing = 0.sp,
     ),
     headlineLarge = TextStyle(
+        fontFamily = JostFont,
         fontWeight = FontWeight.Normal,
         fontSize = 32.sp,
         lineHeight = 40.sp,
         letterSpacing = 0.sp,
     ),
     headlineMedium = TextStyle(
+        fontFamily = JostFont,
         fontWeight = FontWeight.Normal,
         fontSize = 28.sp,
         lineHeight = 36.sp,
         letterSpacing = 0.sp,
     ),
     headlineSmall = TextStyle(
+        fontFamily = JostFont,
         fontWeight = FontWeight.Normal,
         fontSize = 24.sp,
         lineHeight = 32.sp,
         letterSpacing = 0.sp,
+        lineHeightStyle = LineHeightStyle(
+            alignment = Alignment.Bottom,
+            trim = Trim.None,
+        ),
     ),
     titleLarge = TextStyle(
+        fontFamily = JostFont,
         fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp,
+        lineHeightStyle = LineHeightStyle(
+            alignment = Alignment.Bottom,
+            trim = Trim.LastLineBottom,
+        ),
     ),
     titleMedium = TextStyle(
+        fontFamily = JostFont,
         fontWeight = FontWeight.Bold,
         fontSize = 18.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.1.sp,
     ),
     titleSmall = TextStyle(
+        fontFamily = JostFont,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp,
     ),
+    // Default text style
     bodyLarge = TextStyle(
+        fontFamily = JostFont,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp,
+        lineHeightStyle = LineHeightStyle(
+            alignment = Alignment.Center,
+            trim = Trim.None,
+        ),
     ),
     bodyMedium = TextStyle(
+        fontFamily = JostFont,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.25.sp,
     ),
     bodySmall = TextStyle(
+        fontFamily = JostFont,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.4.sp,
     ),
+    // Used for Button
     labelLarge = TextStyle(
+        fontFamily = JostFont,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp,
+        lineHeightStyle = LineHeightStyle(
+            alignment = Alignment.Center,
+            trim = Trim.LastLineBottom,
+        ),
     ),
+    // Used for Navigation items
     labelMedium = TextStyle(
+        fontFamily = JostFont,
         fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp,
+        lineHeightStyle = LineHeightStyle(
+            alignment = Alignment.Center,
+            trim = Trim.LastLineBottom,
+        ),
     ),
+    // Used for Tag
     labelSmall = TextStyle(
+        fontFamily = JostFont,
         fontWeight = FontWeight.Medium,
         fontSize = 10.sp,
-        lineHeight = 16.sp,
+        lineHeight = 14.sp,
         letterSpacing = 0.sp,
+        lineHeightStyle = LineHeightStyle(
+            alignment = Alignment.Center,
+            trim = Trim.LastLineBottom,
+        ),
     ),
 )
