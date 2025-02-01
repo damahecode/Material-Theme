e85f55bdd18a30a915c6ef5e266d7f7818573543
Damahe Code <damahecode@gmail.com>
2025-02-01 17:07:08 +0530

    add Material Theme colors

---
+++ core/material-theme/src/main/java/com/code/damahe/material/theme/Color.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2024 damahecode.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package com.code.damahe.material.theme
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * Material Theme colors.
+ */
+internal val Blue10 = Color(0xFF001F28)
+internal val Blue20 = Color(0xFF003544)
+internal val Blue30 = Color(0xFF004D61)
+internal val Blue40 = Color(0xFF006780)
+internal val Blue80 = Color(0xFF5DD5FC)
+internal val Blue90 = Color(0xFFB8EAFF)
+internal val DarkPurpleGray10 = Color(0xFF201A1B)
+internal val DarkPurpleGray20 = Color(0xFF362F30)
+internal val DarkPurpleGray90 = Color(0xFFECDFE0)
+internal val DarkPurpleGray95 = Color(0xFFFAEEEF)
+internal val DarkPurpleGray99 = Color(0xFFFCFCFC)
+internal val Orange10 = Color(0xFF380D00)
+internal val Orange20 = Color(0xFF5B1A00)
+internal val Orange30 = Color(0xFF812800)
+internal val Orange40 = Color(0xFFA23F16)
+internal val Orange80 = Color(0xFFFFB59B)
+internal val Orange90 = Color(0xFFFFDBCF)
+internal val Purple10 = Color(0xFF36003C)
+internal val Purple20 = Color(0xFF560A5D)
+internal val Purple30 = Color(0xFF702776)
+internal val Purple40 = Color(0xFF8B418F)
+internal val Purple80 = Color(0xFFFFA9FE)
+internal val Purple90 = Color(0xFFFFD6FA)
+internal val PurpleGray30 = Color(0xFF4D444C)
+internal val PurpleGray50 = Color(0xFF7F747C)
+internal val PurpleGray60 = Color(0xFF998D96)
+internal val PurpleGray80 = Color(0xFFD0C3CC)
+internal val PurpleGray90 = Color(0xFFEDDEE8)
+internal val Red10 = Color(0xFF410002)
+internal val Red20 = Color(0xFF690005)
+internal val Red30 = Color(0xFF93000A)
+internal val Red40 = Color(0xFFBA1A1A)
+internal val Red80 = Color(0xFFFFB4AB)
+internal val Red90 = Color(0xFFFFDAD6)
